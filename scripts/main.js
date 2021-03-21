e4fec43234d392a4463771263269d0fafcd8e8ab
haotianjia95 <79532238+haotianjia95@users.noreply.github.com>
2021-03-21 19:37:17 -0400

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -15,14 +15,14 @@ let myHeading = document.querySelector('h1');
 function setUserName() {
   let myName = prompt('请输入你的名字。');
   localStorage.setItem('name', myName);
-  myHeading.textContent = 'Mozilla 酷毙了，' + myName;
+  myHeading.textContent = '你好，' + myName;
 }
 
 if(!localStorage.getItem('name')) {
   setUserName();
 } else {
   let storedName = localStorage.getItem('name');
-  myHeading.textContent = '点击图片' + storedName;
+  myHeading.textContent = '你好,' + storedName;
 }
 
 myButton.onclick = function() {
